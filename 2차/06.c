d21c05f09f1bb73566fb301a4527c09cab844d8d
Hajin Kim <rhrnak624@naver.com>
2021-05-30 20:31:46 +0900

    0530

---
+++ 2차/06.c
@@ -0,0 +1,10 @@
+int solution(int floors[], int floors_len) {
+    int dist = 0;
+    for(int i = 1; i<floors_len; ++i){
+        if(floors[i] > floors[i-1])
+            dist += floors[i] - floors[i-1];
+        else
+            dist += floors[i-1] - floors[i];
+    }
+    return dist;
+}
